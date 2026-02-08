2c8024c993b4c52827add1173a84b31f0b0ab783
Arisha-ali <Arishaali0998@gmail.com>
2026-02-08 22:58:00 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverActions: {}, // Enable server actions if needed for future functionality
+
+  },
+};
+
+module.exports = nextConfig;
